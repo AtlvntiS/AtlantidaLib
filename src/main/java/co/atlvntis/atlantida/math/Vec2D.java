c0f97adf51f4797615a708749e45995be7bd7e42
atlvntis <Jvgc2001!>
2020-02-15 20:15:54 -0300

    Math added

---
+++ src/main/java/co/atlvntis/atlantida/math/Vec2D.java
@@ -0,0 +1,13 @@
+package co.atlvntis.atlantida.math;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class Vec2D {
+
+    private final int x;
+    private final int z;
+
+}
---
+++ src/main/java/co/atlvntis/atlantida/math/Vec3D.java
@@ -0,0 +1,14 @@
+package co.atlvntis.atlantida.math;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class Vec3D {
+
+    private final int x;
+    private final int y;
+    private final int z;
+
+}
