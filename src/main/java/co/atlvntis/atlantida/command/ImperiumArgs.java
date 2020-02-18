de401297c3df32293946113b9ab97c0438cc459b
atlvntis <Jvgc2001!>
2020-02-18 20:46:49 -0300

    Imperium bugs fixed.

--- src/main/java/co/atlvntis/atlantida/command/ImperiumArgs.java
+++ src/main/java/co/atlvntis/atlantida/command/ImperiumArgs.java
@@ -68,7 +68,7 @@ public class ImperiumArgs {
 
         List<ImperiumArg> argMap = new ArrayList<>();
 
-        for(int i = 0; i < args.length -1; i++) {
+        for(int i = 0; i < args.length && i < imperium.minArgs(); i++) {
 
             if(i < imperium.getOptParameters().size() - 1) {
                 argMap.add(new ImperiumArg(imperium.getOptParameters().get(i), args[i]));
